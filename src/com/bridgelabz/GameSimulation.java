2b9eb63f646c0dc2557ee62c6fe04bddc72bd5a5
Sheralram <ramkrishna.sheral@gmail.com>
2021-08-13 09:25:42 +0530

    INITIALISING STAKE AND BET

--- src/com/bridgelabz/GameSimulation.java
+++ src/com/bridgelabz/GameSimulation.java
@@ -1,7 +1,10 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class GameSimulation {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambler Simulation");
-    }
+        
+           }
 }
---
+++ src/com/bridgelabz/com/Bridgelabz/utilityclass.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.com.Bridgelabz;
+
+public class utilityclass {
+    public static final int stake=100;
+    public static final int bet=1;
+
+    //public void Gambler{
+
+
+    //}
+}
